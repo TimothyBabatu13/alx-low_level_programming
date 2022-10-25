29b43c8c9427afdd2c849ce3e702e8c80aaba2a5
TimothyBabatu13 <knbtimothy@gmail.com>
2022-10-25 09:28:09 -0700

    This is the end of today task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- **puts_half - puts half of a string
- **@str: string
- **Return:void
+ *puts_half - puts half of a string
+ *@str: string
+ *Return:void
  */
 void puts_half(char *str)
 {
@@ -24,17 +24,18 @@ void puts_half(char *str)
 
 
 /**
- **_strlen - returns the length of a string
- ** @s: string
- **Return: returns lenght;
+ *_strlen - returns the length of a string
+ *@s: string
+ *Return: returns lenght;
  */
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
 
 	return (inc);
 }
-i
+
