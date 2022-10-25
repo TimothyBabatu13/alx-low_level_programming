d2241ffde7fa8313c722724a9845c9fb4c503f77
TimothyBabatu13 <knbtimothy@gmail.com>
2022-10-25 09:23:50 -0700

    L

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,7 @@
 void print_rev(char *s)
 {
 	int len = 0, index = 0;
+
 	len = _strlen(s);
 
 	for (index = len - 1; index >= 0; index--)
@@ -16,13 +17,14 @@ void print_rev(char *s)
 }
 
 /**
- *  *_strlen - returns the length of a string
- *   * @s: string
- *    *Return: returns lenght;
- *     */
+ *_strlen - returns the length of a string
+ *@s: string
+ *Return: returns lenght;
+ */
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
